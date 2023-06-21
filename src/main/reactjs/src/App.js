1b11443623ac2c10c80d6c9ae0561d6dfc747ad4
jihyunnee <ldlwlgus0076@gmail.com>
2023-06-21 16:07:17 +0900

    0621

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import img from './귀엽쿼카.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <img alt ='' src={img}/>
+      <h2>docker 배포 성공기원!!!</h2>
+    </div>
+  );
+}
+
+export default App;
